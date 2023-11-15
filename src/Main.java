60ae80afc3b9892e4ea22c31f3d6507257e5a50f
Alina Chichirova <privet-alina2014@yandex.ru>
2023-11-15 17:36:20 +0300

    Lesson14

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String s = "Фото";
+
+        try (FileOutputStream outputStream = new FileOutputStream("hello.png")) {
+            byte[] bytes =s.getBytes();
+            outputStream.write(bytes,0,bytes.length);
+            System.out.println("Успешно");
+            } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
